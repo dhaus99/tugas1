b3636ff640eecfcd48b03bcd5a3e719f6a55a55c
dhaus99 <bayudede98@gmail.com>
2017-05-24 05:46:34 -0500

    Hitung Luas Lingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -1,14 +1,10 @@
-class HitungLuas2 {
-
-	public static int cariLuasPersegi(int panjang, int lebar){
+class HitungLuas2{
+	
+	public static void cariLuasPersegi(int panjang, int lebar){
 		return panjang * lebar;
 	}
-	
-	public static double cariLuasLingkaran (double r){
-		return (22/7) * r * r;
-	}
-	
-	public static void maint (String [] args){
-	
+
+	public static void main(String[] args){
+
 	}
 }
